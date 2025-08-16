1f2212737d82d50fcbba889da1df2756fa5f31f8
Mostafa Raihan <mostafaraihan.bd@gmail.com>
2025-08-16 12:36:53 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-#pip install pytesseract
-#pip install pillow
 import pytesseract
-from  PIL import Image
+from PIL import Image
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+# Set Tesseract path (only required for Windows)
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-img_path='card.png'
-img_open=Image.open(img_path)
-text=pytesseract.image_to_string(img_open,)
+# Input image
+img_path = "card.png"
+img_open = Image.open(img_path)
 
+# Extract text
+text = pytesseract.image_to_string(img_open)
+
+print("Extracted Text:\n")
 print(text)
